26e01d0ab3a98544b6a9a7d763e0ceb8a6f27691
Christian Clauss <cclauss@me.com>
2019-07-31 22:13:27 +0200

    Spaces -> Tabs in main_att.py

--- Algorithm-code/RelationExtraction_CNN+ATT/main_att.py
+++ Algorithm-code/RelationExtraction_CNN+ATT/main_att.py
@@ -10,7 +10,6 @@ from utils import save_pr, now, eval_metric
 
 
 def collate_fn(batch):
-
 	data, label = zip(*batch)
 	return data, label
 
@@ -18,44 +17,44 @@ def test(**kwargs):
 	pass
 
 def train(**kwargs):
-    kwargs.update({'model': 'PCNN_ATT'})
-    opt.parse(kwargs)
-    if opt.use_gpu:
+	kwargs.update({'model': 'PCNN_ATT'})
+	opt.parse(kwargs)
+	if opt.use_gpu:
 		torch.cuda.set_device(opt.gpu_id)
 
-    model = getattr(models, 'PCNN_ATT')(opt)
-    if opt.use_gpu:
+	model = getattr(models, 'PCNN_ATT')(opt)
+	if opt.use_gpu:
 		model.cuda()
 
 	# loading data
-    DataModel = getattr(datasets, opt.data + 'Data')
-    train_data = DataModel(opt.data_root, train=True)
-    train_data_loader = DataLoader(train_data, opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn = collate_fn)
-    
-    test_data = DataModel(opt.data_root, train=False)
-    test_data_loader = DataLoader(test_data, opt.batch_size, shuffle=False, num_workers=opt.num_workers, collate_fn = collate_fn)
-    print('{} train data: {}; test data: {}'.format(now(), len(train_data), len(test_data)))
+	DataModel = getattr(datasets, opt.data + 'Data')
+	train_data = DataModel(opt.data_root, train=True)
+	train_data_loader = DataLoader(train_data, opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn = collate_fn)
 
-    optimizer = optim.Adadelta(model.parameters(), rho=0.95, eps=1e-6)
+	test_data = DataModel(opt.data_root, train=False)
+	test_data_loader = DataLoader(test_data, opt.batch_size, shuffle=False, num_workers=opt.num_workers, collate_fn = collate_fn)
+	print('{} train data: {}; test data: {}'.format(now(), len(train_data), len(test_data)))
+
+	optimizer = optim.Adadelta(model.parameters(), rho=0.95, eps=1e-6)
 
 
-    # train
-    for epoch in range(opt.num_epochs):
+	# train
+	for epoch in range(opt.num_epochs):
 		total_loss = 0
 		for idx, (data, label_set) in enumerate(train_data_loader):
 
-		    label = [l[0] for l in label_set]
+			label = [l[0] for l in label_set]
 
-		    optimizer.zero_grad()
-		    model.batch_size = opt.batch_size
-		    loss = model(data, label)
-		    if opt.use_gpu:
-		        label = torch.LongTensor(label).cuda()
-		    else:
-		        label = torch.LongTensor(label)
-		    loss.backward()
-		    optimizer.step()
-		    total_loss += loss.item()
+			optimizer.zero_grad()
+			model.batch_size = opt.batch_size
+			loss = model(data, label)
+			if opt.use_gpu:
+				label = torch.LongTensor(label).cuda()
+			else:
+				label = torch.LongTensor(label)
+			loss.backward()
+			optimizer.step()
+			total_loss += loss.item()
 
 		if epoch > 2:
 			pred_res, p_num = predict_var(model, test_data_loader)
@@ -145,6 +144,3 @@ def predict(model, test_data_loader):
 if __name__ == '__main__':
 	import fire
 	fire.Fire()
-		
-
-
